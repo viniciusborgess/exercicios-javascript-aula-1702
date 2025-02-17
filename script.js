edbf9cbf33478400e789924d772d7122e90ede47
Vinicius Borges <viniciussobreiraborges@hotmail.com>
2025-02-17 09:22:01 -0300

    Exercício 04 finalizado

--- script.js
+++ script.js
@@ -9,3 +9,8 @@ console.log(dividir(9, 3))
 //EX 03
 const mensagemBoasVindas = (nome) => "Seja bem-vindo," + nome
 console.log(mensagemBoasVindas(" Vinicius!"))
+
+//EX 04
+const aplicarOperacao = (a, b, operacao) => operacao(a, b);
+console.log(aplicarOperacao(5, 10, (x, y) => x + y));
+console.log(aplicarOperacao(5, 4, (x, y) => x * y));

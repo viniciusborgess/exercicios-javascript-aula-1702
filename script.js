7ae3b4dc10e1255c34bcb00deeb2854e9e20d1b1
Vinicius Borges <viniciussobreiraborges@hotmail.com>
2025-02-17 10:17:32 -0300

    Exercício 07 finalizado

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ console.log(dobro(2));
 
 //EX 02
 const dividir = (a, b) => a / b;
-console.log(dividir(9, 3))
+console.log(dividir(9, 3));
 
 //EX 03
-const mensagemBoasVindas = (nome) => "Seja bem-vindo," + nome
-console.log(mensagemBoasVindas(" Vinicius!"))
+const mensagemBoasVindas = (nome) => "Seja bem-vindo," + nome;
+console.log(mensagemBoasVindas(" Vinicius!"));
 
 //EX 04
 const aplicarOperacao = (a, b, operacao) => operacao(a, b);
@@ -21,6 +21,11 @@ const precosComDesconto = precos.map(valores => 10 / 100 * valores);
 console.log(precosComDesconto);
 
 //EX 06
-const idades = [12, 20, 15, 18, 43, 32]
+const idades = [12, 20, 15, 18, 43, 32];
 const maioresDeIdade = idades.filter(idade => idade >= 18);
-console.log(maioresDeIdade)
+console.log(maioresDeIdade);
+
+//EX 07
+const numeros = [20, 11, 30, 40];
+const soma = numeros.reduce((total, num) => total + num, 0);
+console.log(soma)
